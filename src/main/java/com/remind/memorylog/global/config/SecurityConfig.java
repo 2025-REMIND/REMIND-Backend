823fd22770b32907f17dff0f531f5f0fe5868c43
gimn70009 <risa1110@hansung.ac.kr>
2025-05-21 09:58:59 +0900

    refactor: SecurityConfig에서 filterChain 부분 제거

--- src/main/java/com/remind/memorylog/global/config/SecurityConfig.java
+++ src/main/java/com/remind/memorylog/global/config/SecurityConfig.java
@@ -3,26 +3,12 @@ package com.remind.memorylog.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/signup", "/signin").permitAll()
-                        .anyRequest().authenticated() // 나머지는 인증 필요
-                )
-                .build();
-    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
