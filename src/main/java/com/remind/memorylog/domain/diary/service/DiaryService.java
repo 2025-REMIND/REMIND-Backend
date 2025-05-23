a97bc4d111f28896bcd09a06ea35d3dae21e326b
gimn70009 <risa1110@hansung.ac.kr>
2025-05-23 14:02:58 +0900

    feat: 기록된 기억 목록 조회 API 개발

--- src/main/java/com/remind/memorylog/domain/diary/service/DiaryService.java
+++ src/main/java/com/remind/memorylog/domain/diary/service/DiaryService.java
@@ -5,7 +5,9 @@ import com.remind.memorylog.domain.diary.web.dto.DiaryRecordResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface DiaryService {
     DiaryRecordResponse recordMemory(DiaryRecordRequest diaryRequest, MultipartFile image) throws IOException;
+    List<DiaryRecordResponse> getRecentDiaries(Long memberId);
 }
