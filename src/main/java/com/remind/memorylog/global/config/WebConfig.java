b1a93a622e3e75e377b5cceaa7fc2e31e64f7d66
gimn70009 <risa1110@hansung.ac.kr>
2025-05-29 15:37:11 +0900

    feat: 프론트 배포

--- src/main/java/com/remind/memorylog/global/config/WebConfig.java
+++ src/main/java/com/remind/memorylog/global/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 이 서버의 모든 API 요청에 대해 CORS 허용 설정 적용
-                .allowedOrigins("http://localhost:3000") // 이 주소에서 온 요청은 허용(실제 프론트 주소)
+                .allowedOrigins("http://localhost:3000", "https://remindteam.netlify.app") // 이 주소에서 온 요청은 허용(실제 프론트 주소)
                 .allowedMethods("GET", "POST", "PUT","DELETE","OPTIONS") // 이와 같은 메서드 허용
                 .allowCredentials(true); // 쿠키, 세션 같은 인증 정보 포함도 허용. true로 해야 로그인 정보나 사용자 상태 유지 같은 기능이 제대로 동작함
     }
